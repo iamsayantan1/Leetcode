e2e061e83806ebf329c5a5868e54e27e2726ca2a
Sayantan Das <67505340+iamsayantan1@users.noreply.github.com>
2025-09-08 21:45:19 +0530

    Create pascals-triangle-optimized.java
    Optimized solution

---
+++ 118-pascals-triangle/pascals-triangle-optimized.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1); // first element is always 1
+
+            // fill middle values using previous row
+            for (int j = 1; j < i; j++) {
+                int val = result.get(i - 1).get(j - 1) + result.get(i - 1).get(j);
+                row.add(val);
+            }
+
+            if (i > 0) {
+                row.add(1); // last element is always 1
+            }
+
+            result.add(row);
+        }
+
+        return result;
+    }
+}
