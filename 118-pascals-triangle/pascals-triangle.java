a9f372159bb47d5664778f2a8d4135b83e0bfc94
Sayantan Das <67505340+iamsayantan1@users.noreply.github.com>
2025-09-12 23:35:11 +0530

    Time: 1 ms (86.57%) | Memory: 42.1 MB (60.39%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,32 +1,27 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        int[][] pascalTriangle = new int[numRows][];
-
-        for(int row=0 ; row<numRows ; row++) {
-            int[] indexArr = new int[row+1];
-            indexArr[0] = 1;
-            indexArr[row] = 1;
-            pascalTriangle[row] = indexArr;
+        int[][] pascal = new int[numRows][];
+        for(int row=0;row<numRows;row++) {
+            pascal[row] = new int[row+1];
+            pascal[row][0] = 1;
+            pascal[row][row] = 1;
         }
 
-        if (numRows > 2) {
-            for(int row=1 ; row<numRows ; row++) {
-                for(int col=1 ; col<row ; col++) {
-                    pascalTriangle[row][col] = pascalTriangle[row-1][col-1] + pascalTriangle[row-1][col];
-                }
+        for(int row=2;row<numRows;row++) {
+            for(int col=1;col<=row-1;col++) {
+                pascal[row][col] = pascal[row-1][col] + pascal[row-1][col-1];
             }
         }
 
-        List<List<Integer>> pascal = new ArrayList<>();
-
-        for (int[] innerArr : pascalTriangle) {
-            List<Integer> tempArr = new ArrayList<>();
-            for (int elem : innerArr) {
-                tempArr.add(elem);
+        List<List<Integer>> pascalTri = new ArrayList<>();
+        for(int row=0;row<numRows;row++) {
+            List<Integer> innerArr = new ArrayList<>();
+            for(int col=0;col<=row;col++) {
+                innerArr.add(pascal[row][col]);
             }
-            pascal.add(tempArr);
+            pascalTri.add(innerArr);
         }
 
-        return pascal;
+        return pascalTri;
     }
 }
