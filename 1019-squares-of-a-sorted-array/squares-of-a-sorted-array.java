d40bf02a415d356dc328d5f13ae20dfe9af07e70
Sayantan Das <67505340+iamsayantan1@users.noreply.github.com>
2026-02-14 00:35:02 +0530

    Time: 1 ms (99.81%) | Memory: 47.4 MB (63.34%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,38 +1,42 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] result = new int[nums.length];
+        int len = nums.length;
 
-        int left=nums.length-1, right=nums.length;
-        for(int index=0 ; index<nums.length ; index++) {
-            if(nums[index] >= 0) {
-                left = index-1;
-                right = index;
+        int[] out = new int[len];
+        int left = -1, right = len;
+        // Find mid
+        for(int index=0;index<len;index++) {
+            if(nums[index]<0) {
+                left=index;
+            }
+            else {
+                right=index;
                 break;
             }
         }
-
-        int resultIdx=0;
-        while(left != -1 || right != nums.length) {
-            if(left != -1 && right != nums.length) {
-                if(-1*nums[left] > nums[right]) {
-                    result[resultIdx++] = nums[right]*nums[right];
-                    right++;
-                }
-                else {
-                    result[resultIdx++] = nums[left]*nums[left];
-                    left--;
-                }
+        
+        int index=0;
+        while(left != -1 || right != len) {
+            if(left == -1) {
+                out[index++] = nums[right]*nums[right];
+                right++;
             }
-            else if(left != -1) {
-                result[resultIdx++] = nums[left]*nums[left];
+            else if(right == len) {
+                out[index++] = nums[left]*nums[left];
                 left--;
             }
             else {
-                result[resultIdx++] = nums[right]*nums[right];
-                right++;
+                if(Math.abs(nums[left])<nums[right]) {
+                    out[index++] = nums[left]*nums[left];
+                    left--;
+                }
+                else {
+                    out[index++] = nums[right]*nums[right];
+                    right++;
+                }
             }
         }
 
-        return result;
+        return out;
     }
 }
