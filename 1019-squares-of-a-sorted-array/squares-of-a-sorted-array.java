4757434e766f189501e159d023b360acef446a2d
Sayantan Das <67505340+iamsayantan1@users.noreply.github.com>
2026-02-14 10:38:13 +0530

    Time: 1 ms (99.81%) | Memory: 47.2 MB (86.88%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -3,37 +3,17 @@ class Solution {
         int len = nums.length;
 
         int[] out = new int[len];
-        int left = -1, right = len;
-        // Find mid
-        for(int index=0;index<len;index++) {
-            if(nums[index]<0) {
-                left=index;
-            }
-            else {
-                right=index;
-                break;
-            }
-        }
+        int left = 0, right = len-1;
         
-        int index=0;
-        while(left != -1 || right != len) {
-            if(left == -1) {
-                out[index++] = nums[right]*nums[right];
-                right++;
-            }
-            else if(right == len) {
-                out[index++] = nums[left]*nums[left];
-                left--;
+        int index = len-1;
+        while(left<=right) {
+            if(-1*nums[left]>nums[right]) {
+                out[index--] = nums[left]*nums[left];
+                left++;
             }
             else {
-                if(Math.abs(nums[left])<nums[right]) {
-                    out[index++] = nums[left]*nums[left];
-                    left--;
-                }
-                else {
-                    out[index++] = nums[right]*nums[right];
-                    right++;
-                }
+                out[index--] = nums[right]*nums[right];
+                right--;
             }
         }
 
