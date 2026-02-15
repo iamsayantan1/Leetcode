0598f591ef755f81318c1c84ee69292b41e0bb0e
Sayantan Das <67505340+iamsayantan1@users.noreply.github.com>
2026-02-15 10:55:23 +0530

    Time: 55 ms (36.44%) | Memory: 99.4 MB (24.03%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,26 +1,25 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        int left=0, right=0;
-        Set<Integer> set = new HashSet<>();
-        long sum=0;
-        long maxsum=0;
+        int left=0;
+        long sum=0,maxsum=0;
+        Map<Integer,Integer> freq = new HashMap<>();
 
-        while(right<nums.length) {
+        for(int right=0;right<nums.length;right++) {
             int rElem = nums[right];
+            freq.put(rElem,freq.getOrDefault(rElem,0)+1);
             sum += rElem;
-
-            while(set.contains(rElem) || right-left+1>k) {
+            if(right - left + 1 > k) {
                 int lElem = nums[left++];
+                freq.put(lElem,freq.get(lElem)-1);
+                if (freq.get(lElem) == 0) {
+                    freq.remove(lElem);
+                }
                 sum -= lElem;
-                set.remove(lElem);
             }
             
-            if(k==right-left+1) {
+            if(k==right-left+1 && freq.size()==k) {
                 maxsum = Math.max(maxsum,sum);
             }
-
-            set.add(rElem);
-            right++;
         }
 
         return maxsum;
